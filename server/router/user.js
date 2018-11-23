f51a65875a4306721d304c50a95fb67e02eaea37
CHENHUI <609630780@qq.com>
2018-11-23 20:59:10 +0800

    修改了接口

--- server/router/user.js
+++ server/router/user.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 const jwsSecret = config.jwsSecret
 var moment = require('moment');
+const crypto = require('crypto');
 
 /**
  * 通过openid 获取用户信息
@@ -35,7 +36,7 @@ module.exports = router.post("/:db/user/addUser", async ctx => {
     let  params = ctx.params
     let  user = ctx.request.body
     let obj=await db.insert(params.db,"user",user)
-    //console.log("----obj.insertedCount->"+obj.insertedCount)
+
     if(obj.insertedCount>0){
         result.success=true
         result.code=0
@@ -114,16 +115,43 @@ module.exports = router.post("/:db/order/addOrder", async ctx => {
  * 获取推荐信息列表
  */
 module.exports = router.post("/:db/order/getOrderList", async ctx => {
-    let  result = {success: false, msg: "",data: [] ,code:-1 }
+    let  result = {success: false, msg: "",data: [] ,code:-1,one:0,two:0,three:0,four:0 }
     let  params = ctx.params
     let  orders = ctx.request.body
-    let obj=await db.findTableInfo(params.db,"order",orders)
+    let obj=await db.findTableList(params.db,"order",orders)
 
     if(obj.length>0){
+        result.one=obj.length;
+
+        let paramfilter={openid:orders.openid,uid:orders.uid,recommended_state:"2"}
+        let objState=await db.findTableList(params.db,"order",paramfilter)
+        result.two=objState.length;
+
+
+        paramfilter={openid:orders.openid,uid:orders.uid,recommended_state:"3"}
+        objState=await db.findTableList(params.db,"order",paramfilter)
+        result.three=objState.length;
+
+        paramfilter={openid:orders.openid,uid:orders.uid,recommended_state:"4"}
+        objState=await db.findTableList(params.db,"order",paramfilter)
+        result.four=objState.length;
+
+        var o;
+        var arrayObj = new Array();
+        obj.forEach(function(item,index){
+            let pickDate = moment(item.end_date);
+            let diff = pickDate.diff(moment(), 'days');//相差几天
+            console.log("========"+diff)
+             item = Object.assign(item,{days:diff})
+            o=item;
+            arrayObj.push(o)
+        })
+        console.log("===ddddd====="+o.days)
+        console.log("===dddd====="+arrayObj)
         result.success=true
         result.code=0
         result.msg="获取信息成功"
-        result.data = obj
+        result.data = arrayObj
     }else{
         result.msg="未找到数据"
         result.data = obj
@@ -136,7 +164,7 @@ module.exports = router.post("/:db/order/getOrderList", async ctx => {
  */
 
 module.exports = router.post('/:db/order/getOrderById', async ctx => {
-    let result = {success: false, msg: '', data: null ,code:-1 }
+    let result = {success: false, msg: '', data: [] ,code:-1 }
     let params = ctx.params
     let  orders = ctx.request.body
     let id=orders.orderid
@@ -156,12 +184,12 @@ module.exports = router.post('/:db/order/getOrderById', async ctx => {
  * 修改推荐信息
  */
 module.exports = router.post('/:db/order/updateOrder', async ctx => {
-    let result = {success: false, msg: '', data: null,code:-1  }
+    let result = {success: false, msg: '', data: [],code:-1  }
     let params = ctx.params
     let updateData = ctx.request.body
 
-    let obj=await db.updata(params.db,"order",updateData.orderid,updateData)
-    if(obj.modifiedCount>0){
+    let obj=await db.updata(params.db,"order",updateData.orderId,updateData)
+    if(obj){
         result.success=true
         result.msg='更新成功'
         result.code=0
@@ -175,7 +203,7 @@ module.exports = router.post('/:db/order/updateOrder', async ctx => {
 
 //必带参数 pageNum ,pageSize, filter    不传pageNum or pageSize 当做全部查
 module.exports = router.get('/:db/api/:table',async ctx =>{
-    let result = {success: false, msg: '', data: null,code:-1  ,total:0}
+    let result = {success: false, msg: '', data: [],code:-1  ,total:0}
     let params = ctx.params
     let query = ctx.query
 
@@ -254,20 +282,22 @@ module.exports = router.post("/:db/policy/getPolicy", async ctx => {
     ctx.body = result
 })
 
+
+
+
 //--------------------参加活动 end---------------------------------
 
 //--------------------推荐奖励说明 start---------------------------------
 /**
- * 添加参加活动表
+ * 添加推荐奖励说明
  */
 module.exports = router.post("/:db/reward/addReward", async ctx => {
     let  result = {success: false, msg: "",data: [] ,code:-1 }
     let  params = ctx.params
     let  insertData = ctx.request.body
-    /*//推荐状态 1套餐90天活动
-    insertData = Object.assign(insertData,{policy_type:"1"})*/
+
     let obj=await db.insert(params.db,"reward",insertData)
-    //console.log("-obj.insertedCount-"+obj.insertedCount)
+
     if(obj.insertedCount){
         result.success=true
         result.msg="插入成功"
@@ -281,7 +311,7 @@ module.exports = router.post("/:db/reward/addReward", async ctx => {
 
 
 /**
- * 获取参加活动表
+ * 获取推荐奖励说明
  */
 module.exports = router.post("/:db/reward/getRewards", async ctx => {
     let  result = {success: false, msg: "",data: [],code:-1 }
@@ -301,6 +331,46 @@ module.exports = router.post("/:db/reward/getRewards", async ctx => {
     ctx.body = result
 })
 
+
+/**
+ * 修改推荐奖励说明
+ */
+module.exports = router.post('/:db/reward/updateReward', async ctx => {
+    let result = {success: false, msg: '', data: [],code:-1  }
+    let params = ctx.params
+    let updateData = ctx.request.body
+
+    let obj=await db.updata(params.db,"reward",updateData.rewardId,updateData)
+    if(obj){
+        result.success=true
+        result.msg='更新成功'
+        result.code=0
+    }else{
+        result.msg='更新失败'
+    }
+    ctx.body = result
+})
+
+
+/**
+ * 删除推荐奖励说明
+ */
+module.exports = router.post('/:db/reward/delReward', async ctx => {
+    let result = {success: false, msg: '', data: [],code:-1 }
+    let params = ctx.params
+    let delData = ctx.request.body
+    let obj=await db.remove(params.db,"reward",delData.rewardId)
+    if(obj.value){
+        result.success=true
+        result.msg='删除成功'
+        result.code=0
+    }else{
+        result.msg='删除失败'
+        result.code=-1
+    }
+    result.data = obj.value
+    ctx.body = result
+})
 //--------------------推荐奖励说明 end---------------------------------
 
 /**
@@ -319,3 +389,210 @@ module.exports = router.post("/:db/intervalDays", async ctx => {
 
     ctx.body = result
 })
+
+//------------------------------------------------------------------------
+
+/**
+ * 新增管理员用户
+ */
+module.exports = router.post("/:db/admin/addAdminUser", async ctx => {
+    let  result = {success: false, msg: "",data: [] ,code:-1 }
+    let  params = ctx.params
+    let  insertData = ctx.request.body
+    var md5 = crypto.createHash('md5')
+
+    var cryptedPassword = md5.update(insertData.password).digest('hex');
+    insertData = Object.assign(insertData,{password:cryptedPassword})
+    let obj=await db.insert(params.db,"admin",insertData)
+
+    if(obj.insertedCount){
+        result.success=true
+        result.msg="插入成功"
+        result.data=obj.ops[0]._id
+        result.code=0
+    }else{
+        result.msg="插入失败"
+    }
+    ctx.body = result
+})
+
+
+/**
+ * 更新数据
+ */
+module.exports = router.post("/:db/admin/updateAdminUser", async ctx => {
+    let  result = {success: false, msg: "",data: [] ,code:-1 }
+    let  params = ctx.params
+    let  updateData = ctx.request.body
+    var md5 = crypto.createHash('md5')
+
+    var cryptedPassword = md5.update(updateData.password).digest('hex');
+    updateData = Object.assign(updateData,{password:cryptedPassword})
+
+    let obj=await db.updata(params.db,"admin",updateData.adminid,updateData)
+
+    if(obj){
+        result.success=true
+        result.msg='更新成功'
+        result.code=0
+    }else{
+        result.msg='更新失败'
+    }
+
+    ctx.body = result
+})
+
+/**
+ * 删除推荐奖励说明
+ */
+module.exports = router.post('/:db/admin/delAdminUser', async ctx => {
+    let result = {success: false, msg: '', data: [],code:-1 }
+    let params = ctx.params
+    let delData = ctx.request.body
+    let obj=await db.remove(params.db,"admin",delData.adminid)
+    if(obj.value){
+        result.success=true
+        result.msg='删除成功'
+        result.code=0
+    }else{
+        result.msg='删除失败'
+        result.code=-1
+    }
+    result.data = obj.value
+    ctx.body = result
+})
+
+/**
+ * 管理员登录
+ */
+module.exports = router.post("/:db/admin/login", async ctx => {
+    let result = {success: false, msg: "账号密码错误", code: -1, account: {}}
+    let params = ctx.params
+    let user = ctx.request.body
+
+    var md5 = crypto.createHash('md5')
+    var cryptedPassword = md5.update(user.password).digest('hex');
+
+    user = Object.assign(user,{password:cryptedPassword})
+    let obj=await db.findTableInfo(params.db,"admin",user)
+
+    if(obj.length>0){
+        let profile = {
+            user: user.username,
+            id: 0
+        }
+        result.token = jwt.sign(profile, jwsSecret, {expiresIn: 60 * 60 * 24 * 3  })
+        result.success = true
+        result.code = 0
+        result.msg = "登录成功"
+        result.account.name = obj[0].username
+        result.account._id = obj[0]._id
+    }else{
+        if("ch"==user.username&&"e10adc3949ba59abbe56e057f20f883e"==user.password){
+            let profile = {
+                user: "ch",
+                id: 0
+            }
+            result.token = jwt.sign(profile, jwsSecret, {expiresIn: 60 * 60 * 24 * 3  })
+            result.success = true
+            result.code = 0
+            result.msg = "登录成功"
+            result.account.name ="ch"
+            result.account._id = 0
+        }
+        result.msg="账号密码错误"
+        result.account = obj
+    }
+    ctx.body = result
+
+
+})
+
+//-------------------活动-----------------------
+
+/**
+ * 添加活动
+ */
+module.exports = router.post("/:db/promotion/addPromotion", async ctx => {
+    let  result = {success: false, msg: "",data: [] ,code:-1 }
+    let  params = ctx.params
+    let  insertData = ctx.request.body
+
+    let obj=await db.insert(params.db,"promotion",insertData)
+
+    if(obj.insertedCount){
+        result.success=true
+        result.msg="插入成功"
+        result.data=obj.ops[0]
+        result.code=0
+    }else{
+        result.msg="插入失败"
+    }
+    ctx.body = result
+})
+
+
+/**
+ * 获取活动列表
+ */
+module.exports = router.post("/:db/promotion/getPromotions", async ctx => {
+    let  result = {success: false, msg: "",data: [],code:-1 }
+    let  params = ctx.params
+    let  user = ctx.request.body
+    let obj=await db.findTableInfo(params.db,"promotion",user)
+
+    if(obj.length>0){
+        result.success=true
+        result.code=0
+        result.msg="获取信息成功"
+        result.data = obj
+    }else{
+        result.msg="未找到数据"
+        result.data = obj
+    }
+    ctx.body = result
+})
+
+
+/**
+ * 修改活动列表
+ */
+module.exports = router.post('/:db/promotion/updatePromotion', async ctx => {
+    let result = {success: false, msg: '', data: [],code:-1  }
+    let params = ctx.params
+    let updateData = ctx.request.body
+
+    let obj=await db.updata(params.db,"promotion",updateData.promotionid,updateData)
+    if(obj){
+        result.success=true
+        result.msg='更新成功'
+        result.data = obj.value
+        result.code=0
+    }else{
+        result.msg='更新失败'
+    }
+    ctx.body = result
+})
+
+
+/**
+ * 删除活动
+ */
+module.exports = router.post('/:db/promotion/delPromotion', async ctx => {
+    let result = {success: false, msg: '', data: [],code:-1 }
+    let params = ctx.params
+    let delData = ctx.request.body
+    let obj=await db.remove(params.db,"promotion",delData.promotionid)
+    if(obj.value){
+        result.success=true
+        result.msg='删除成功'
+        result.code=0
+    }else{
+        result.msg='删除失败'
+        result.code=-1
+    }
+    result.data = obj.value
+    ctx.body = result
+})
+
+//--------------------  end---------------------------------
